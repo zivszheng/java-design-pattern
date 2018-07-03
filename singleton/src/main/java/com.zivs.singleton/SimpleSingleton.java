06fb1f678ba53eecda0720c1656ecacc3898c819
zivszheng <zivs.zheng@qq.com>
2018-07-03 13:24:21 +0800

    Update SimpleSingleton.java

--- singleton/src/main/java/com.zivs.singleton/SimpleSingleton.java
+++ singleton/src/main/java/com.zivs.singleton/SimpleSingleton.java
@@ -2,7 +2,7 @@ package com.zivs.singleton;
 
 /**
  * @author zivs.zheng
- * @description: 单列模式 （该类始终只创建一次实例）
+ * @description: 单列模式 （该类始终只创建一次实例,非线程安全）
  * @date 2018/4/22 2:09
  */
 public class SimpleSingleton {
